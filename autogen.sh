d3960a916f33bef04cd725e663a1cec211523f18
Daniel Drake <drake@endlessm.com>
2015-09-11 09:15:02 -0600

    Rename repository to eos-factory-tools
    And move factory test into an appropriately named subdirectory.
    This will now be a more general repository for factory related utilities.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-PROJECT=eos-factory-test
+PROJECT=eos-factory-tools
 builddir=`pwd`
 srcdir=`dirname "$0"`
 [ -z "$srcdir" ] && srcdir=.
